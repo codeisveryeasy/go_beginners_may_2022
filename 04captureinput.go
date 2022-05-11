40abe4460a8747005cbd4e31e68c54415c879d28
codeisveryeasy <prafful@airasia.com>
2022-05-11 06:40:51 +0000

    capture input from stdio

---
+++ 04captureinput.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main()  {
+	//initialize default input device with help of bufio and os standard package
+	bufio.NewReader(os.Stdin)
+	fmt.Print("")
+	
+}
