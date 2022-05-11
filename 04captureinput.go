053de5e0dd03debf2d0b28cb0562e06d14ad0d46
codeisveryeasy <prafful@airasia.com>
2022-05-11 06:53:41 +0000

    read string

--- 04captureinput.go
+++ 04captureinput.go
@@ -6,9 +6,16 @@ import (
 	"os"
 )
 
-func main()  {
+func main() {
 	//initialize default input device with help of bufio and os standard package
-	bufio.NewReader(os.Stdin)
-	fmt.Print("")
-	
+	reader := bufio.NewReader(os.Stdin)
+	//prompt the message on the screen depending on what you want the user to input
+	fmt.Print("Enter you name: ")
+	//use reader to read the string. Hit enter i.e. "\n" (new line) to specify the end of string user wants to enter
+	text, _ := reader.ReadString('\n')
+
+	//print the string entered by user in the console/terminal
+	fmt.Printf("Your name is: %v \n", text)
+	fmt.Printf("Datatype of %v is %T \n", text, text)
+
 }
